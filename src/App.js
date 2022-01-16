566bf46bff20e55a2d01544e28cfd91247b233ed
Bhupendra Singh <bhupe7017@gmail.com>
2022-01-16 18:45:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import "./App.css"
+import ComponentC from './component/ComponentC'
+import { UserProvider } from './component/userContext'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <UserProvider value= 'Vishwash'>
+          <ComponentC />
+        </UserProvider>
+        
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/component/ComponentE.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ComponentE extends Component {
+    render() {
+        return (
+            <div>
+                ComponentE
+            </div>
+        )
+    }
+}
+
+export default ComponentE
